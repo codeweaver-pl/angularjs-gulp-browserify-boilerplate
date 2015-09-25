61ec740f258027d775a7f48bff03ae31fca24bde
Tomasz Zabłocki <tomasz.zablocki@codeweaver.pl>
2015-09-25 07:28:27 +0200

    browserify #2

--- app/js/services/_index.js
+++ app/js/services/_index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var angular = require('angular');
-var bulk = require('bulk-require');
 
 module.exports = angular.module('app.services', []);
 
-bulk(__dirname, ['./**/!(*_index|*.spec).js']);
+var b = require('bulk-require');
+b(__dirname, ['./**/!(*_index|*.spec).js']);
